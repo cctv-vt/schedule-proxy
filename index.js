18afc4b02b39e5a9a7f91809b942c3b58f72cd47
Luna Zeitlyn <luna@gobolin.com>
2021-04-20 15:38:58 -0400

    ✨ express: added favicon

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const xml2js = require('xml2js')
 const axios = require('axios')
+var favicon = require('serve-favicon')
+var path = require('path')
 
 let channel = 9
 
 const app = express()
 
+app.use(favicon(path.join(__dirname, 'favicon.ico')))
+
 app.get('/', (req, res) => {
     axios.get("https://mc.retn.org/xml/program_schedule_feed?channel_id=" + channel).then((rawResponse, err) => {
-        // console.log(res)
+        console.log(rawResponse)
         xml2js.parseString(rawResponse.data, function (err, result) {
             console.log(result)
             res.send(result)
         });
     })
 })
-console.log(process.env.PORT)
+
+console.log("http://localhost:"+process.env.PORT)
 app.listen(process.env.PORT || 80)
