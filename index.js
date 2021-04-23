d53f0be6bde6c53db5861d4a3993933501b59773
Luna Zeitlyn <luna@gobolin.com>
2021-04-22 21:02:52 -0400

    🐛 xml2js: added proccessor to remove ":"

--- index.js
+++ index.js
@@ -12,14 +12,24 @@ app.use(favicon(path.join(__dirname, 'favicon.ico')))
 
 app.get('/', (req, res) => {
     axios.get("https://mc.retn.org/xml/program_schedule_feed?channel_id=" + channel).then((rawResponse, err) => {
-        console.log(rawResponse)
-        xml2js.parseString(rawResponse.data, function (err, result) {
+        let xml = rawResponse.data
+        console.log(xml)
+        xml2js.parseString(xml,{
+            tagNameProcessors: [tagSanitizer],
+            attrValueProcessors: []
+        }, (err, result) => {
+            if (err) console.error(err)
             console.log(result)
             res.header("Access-Control-Allow-Origin", "*");
             res.send(result)
         });
     })
 })
+
+let tagSanitizer = (name) => {
+    console.log(name.replace("psg:","psg_")) 
+    return name.replace("psg:","psg_")
+}
 
 console.log("http://localhost:"+process.env.PORT)
 app.listen(process.env.PORT || 80)
