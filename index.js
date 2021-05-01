5f7a0eaed919a9019dcd67c8b40eae9e5bce50f4
Luna Zeitlyn <luna@gobolin.com>
2021-04-30 20:53:04 -0400

    🎨 added eslit

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const express = require('express')
 const xml2js = require('xml2js')
 const axios = require('axios')
-var favicon = require('serve-favicon')
-var path = require('path')
-
-let channel = 9
+const favicon = require('serve-favicon')
+const path = require('path')
 
 const app = express()
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')))
 
 app.get('/:path', (req, res) => {
-    console.log(req.url)
-    axios.get("https://mc.retn.org/xml" + req.url).then((rawResponse, err) => {
-        let xml = rawResponse.data
-        // console.log(xml)
-        xml2js.parseString(xml,{
-            tagNameProcessors: [tagSanitizer],
-            attrValueProcessors: []
-        }, (err, result) => {
-            if (err) console.error(err)
-            // console.log(result)
-            res.header("Access-Control-Allow-Origin", "*");
-            res.send(result)
-        });
+  console.log(req.url)
+  axios.get('https://mc.retn.org/xml' + req.url).then((rawResponse, err) => {
+    const xml = rawResponse.data
+    // console.log(xml)
+    xml2js.parseString(xml, {
+      tagNameProcessors: [tagSanitizer],
+      attrValueProcessors: []
+    }, (err, result) => {
+      if (err) console.error(err)
+      // console.log(result)
+      res.header('Access-Control-Allow-Origin', '*')
+      res.send(result)
     })
+  })
 })
 
-let tagSanitizer = (name) => {
-    // console.log(name.replace("psg:","psg_")) 
-    return name.replace("psg:","psg_")
+const tagSanitizer = (name) => {
+  // console.log(name.replace("psg:","psg_"))
+  return name.replace('psg:', 'psg_')
 }
 
-console.log("http://localhost:"+process.env.PORT)
+console.log('http://localhost:' + process.env.PORT)
 app.listen(process.env.PORT || 80)
